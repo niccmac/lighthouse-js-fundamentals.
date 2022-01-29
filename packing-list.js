ba18a6d3b8187ced5ed4c7347adce82f5873537b
Nicole MacLean <nicole.mac0404@gmail.com>
2022-01-29 13:20:07 -0800

    add packing list function

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+const packingList = ["bowls","plates","pots","pans","eating utensils","glasses","cups","cooking utensils"];
+
+console.log("Kitchen stuff of pack:");
+
+for(let i = 0; i < packingList.length; i++){
+  console.log(packingList[i]);
+}
+console.log(packingList[3]);
