7fd7f7d994976de8198733579319317277458007
Nicole MacLean <nicole.mac0404@gmail.com>
2022-02-08 09:41:31 -0800

    Adds vols of shapes

---
+++ kata011-BouncyCastles.js
@@ -0,0 +1,56 @@
+const PI = 3.14159;
+
+const sphereVolume = function (radius) {
+  return (4 / 3) * PI * (Math.pow(radius, 3));
+}
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+const coneVolume = function (radius, height) {
+  return PI * (Math.pow(radius, 2)) * (height / 3);
+}
+
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+const prismVolume = function (height, width, depth) {
+  return height * width * depth
+}
+
+console.log(prismVolume(3, 4, 5) === 60);
+
+const totalVolume = function (solids) {
+  let vol = 0;
+  for (let i = 0; i < solids.length; i++) {
+    if (solids[i].type === "sphere") {
+      vol = vol + (sphereVolume(solids[i].radius));
+    } else if (solids[i].type === "cone") {
+      vol = vol + (coneVolume(solids[i].radius, solids[i].height));
+    } else if (solids[i].type === "prism") {
+      vol = vol + (prismVolume(solids[i].height, solids[i].width, solids[i].depth))
+    }
+  }
+  return vol;
+}
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
