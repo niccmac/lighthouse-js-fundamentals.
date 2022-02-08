9199a540302ae1a6a2322af0ff5daf50c360b617
Nicole MacLean <nicole.mac0404@gmail.com>
2022-02-07 16:02:59 -0800

    Add smart parking, updated inst names to js was text file

---
+++ Kata006-SmartParking.js
@@ -0,0 +1,65 @@
+const whereCanIPark = function (spots, vehicle) {
+  let space = [0, 0];
+  for (let c = 0; c < spots.length; c++) {
+    for (let r = 0; r < spots[c].length; r++) {
+      if (vehicle === "motorcycle") {
+        if (spots[c][r] === "R" || spots[c][r] === "S" || spots[c][r] === "M") {
+          space = [[r], [c]];
+          return space;
+        } else {
+          space = false;
+        }
+      } else if (vehicle === "small") {
+        if (spots[c][r] === "R" || spots[c][r] === "S") {
+          space = [[r], [c]];
+          return space;
+        } else {
+          space = false;
+        }
+      } else if (vehicle === "regular") {
+        if (spots[c][r] === "R") {
+          space = [[r], [c]];
+          return space;
+        } else {
+          space = false;
+        }
+      }
+    }
+  }
+  return space;
+};
+  console.log(whereCanIPark(
+    [
+      // COLUMNS ARE X
+      // 0    1    2    3    4    5
+      ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+      ['s', 'M', 's', 'S', 'r', 'M'], // 1
+      ['s', 'M', 's', 'S', 'r', 'm'], // 2
+      ['S', 'r', 's', 'm', 'r', 'M'], // 3
+      ['S', 'r', 's', 'm', 'r', 'M'], // 4
+      ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+    ],
+    'regular'
+  ));
+
+  console.log(whereCanIPark(
+    [
+      ['M', 'M', 'M', 'M'],
+      ['M', 's', 'M', 'M'],
+      ['M', 'M', 'M', 'M'],
+      ['M', 'M', 'r', 'M']
+    ],
+    'small'
+  ));
+
+  console.log(whereCanIPark(
+    [
+      ['s', 's', 's', 's', 's', 's'],
+      ['s', 'm', 's', 'S', 'r', 's'],
+      ['s', 'm', 's', 'S', 'r', 's'],
+      ['S', 'r', 's', 'm', 'r', 's'],
+      ['S', 'r', 's', 'm', 'R', 's'],
+      ['S', 'r', 'S', 'M', 'm', 'S']
+    ],
+    'motorcycle'
+  ));
