faa072ed36ce0a7fa943a7e2dc8854f5a1cf54e0
Nicole MacLean <94476487+niccmac@users.noreply.github.com>
2022-03-10 18:06:36 -0800

    Update LoopyLH.js

--- LoopyLH.js
+++ LoopyLH.js
@@ -1,18 +1,17 @@
-function loopyLighthouse(range, multiples, words) {
-  if (isNaN(range[0]) || isNaN(range[1]) || isNaN(multiples[0]) || isNaN(multiples[1]) || typeof words[0] != 'string' || typeof words[1] != 'string') {
-    return "Argument error"
-  } else {
-    for (let i = range[0]; i <= range[1]; i++) {
-      if (i % multiples[0] === 0 && i % multiples[1] === 0) {
-        console.log(words[0] + words[1])
-      } else if (i % multiples[0] === 0) {
-        console.log(words[0])
-      } else if (i % multiples[1] === 0) {
-        console.log(words[1])
-      } else {
-        console.log(i);
-      }
+
+
+let loopyLighthouse = function (range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
     }
   }
-}
-console.log(loopyLighthouse([1,10],[2,3],["TWO","THREE"]))
+};
+
+console.log(loopyLighthouse([1, 10], [2, 3], ["TWO", "THREE"]));
